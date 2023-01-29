19991b6add6e0b997ab79263ea2b89b2e488635b
HuynhKhuong <thebestman2204@gmail.com>
2023-01-29 22:34:38 +0700

    Create callback function for systick interrupt

--- pltfm/RCC/api/RCC_ex.h
+++ pltfm/RCC/api/RCC_ex.h
@@ -1,7 +1,9 @@
 #ifndef __RCC_EX_H__
 #define __RCC_EX_H__
 
-//declare wrapper to call pre-defined related-RCC exception handler: Systick timer 
+#include "stm32f10x.h"
 
+//declare wrapper which would be called in pre-defined related-RCC exception handler: Systick timer 
+uint8_t Systick_Callback(void);
 
 #endif 
