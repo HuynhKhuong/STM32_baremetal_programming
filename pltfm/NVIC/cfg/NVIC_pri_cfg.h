c5b6538f7235119fbb706474ccf3a4dc2672cdd5
HuynhKhuong <thebestman2204@gmail.com>
2023-03-15 22:18:47 +0700

    Add configuration function to setup interrupt priority order

--- pltfm/NVIC/cfg/NVIC_pri_cfg.h
+++ pltfm/NVIC/cfg/NVIC_pri_cfg.h
@@ -12,7 +12,7 @@ Define structure to configure priorities of interrupts registered by user
 #define NUM_OF_INTERRUPTS_REGISTERED     (uint8_t)3 ///
 
 typedef enum{
-  PRIGROUP_FOUR_ZERO = (uint32_t)0, //pre-emption priority = 4 bits, sub-priority = 1 bit
+  PRIGROUP_FOUR_ZERO = (uint32_t)0, //pre-emption priority = 4 bits, sub-priority = 0 bit
   PRIGROUP_THREE_ONE = (uint32_t)1, 
   PRIGROUP_TWO_TWO = (uint32_t)2, 
   PRIGROUP_ONE_THREE = (uint32_t)3, 
